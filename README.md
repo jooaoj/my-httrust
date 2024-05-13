3a8f1f4f9846c1632898847c78fac49a4c21767e
Jooa Jaakkola <jaakkola.jooa@gmail.coma>
2024-05-13 19:30:53 +0300

    Request and Response

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # my-httrust
+
 A simple http-server built with rust for practise.
+
+## How-to
+
+http-server: program which LISTENS to connections from web-browsers. Browser sends a REQUEST, server gives a RESPONSE!
+1. Listen to REQUESTS (from TCP-traffic)
+2. Handle errors and give RESPONSE (HTTP/1.1 200 OK)
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::io::{ BufRead, Write };
+
+fn handle_connection(mut stream: std::net::TcpStream) {
+    let mut rdr = std::io::BufReader::new(&mut stream);
+    let mut l = String::new();
+    rdr.read_line(&mut l).unwrap();
+
+    match l.trim().split(' ').collect::<Vec<_>>().as_slice() {
+        ["GET", resource, "HTTP/1.1"] => {
+            /*Printing the whole request:
+            loop {
+                rdr.read_line(&mut l).unwrap();
+                if l.trim().is_empty() { 
+                    break;
+                }
+                println!("{l}");
+            }*/
+            let mut p = std::path::PathBuf::new();
+            p.push("public");
+            p.push(resource);
+            stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n<h1>Hello, World!</h1>").unwrap();
+            //stream.write_all(&std::fs::read(p).unwrap()).unwrap();
+        }
+        _ => todo!()
+    }
+}
 fn main() {
-    println!("Hello, World!");
+    let listener = std::net::TcpListener::bind("127.0.0.1:9999").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_connection(stream);
+            }
+            Err(e) => {
+                println!("Connection failed! {e}");
+            }
+        }
+    }
 }
